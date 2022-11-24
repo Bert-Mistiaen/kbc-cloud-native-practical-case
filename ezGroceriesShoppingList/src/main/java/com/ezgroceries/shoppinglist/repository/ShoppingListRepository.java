e03fcb063e9f01070e79d2b4ba71036d0a0cdfd9
bertmistiaen <albert.mistiaen@kbc.be>
2022-11-24 09:13:20 +0100

    lab-02-spring-boot

--- ezGroceriesShoppingList/src/main/java/com/ezgroceries/shoppinglist/repository/ShoppingListRepository.java
+++ ezGroceriesShoppingList/src/main/java/com/ezgroceries/shoppinglist/repository/ShoppingListRepository.java
@@ -1,10 +1,13 @@
 package com.ezgroceries.shoppinglist.repository;
 
 import com.ezgroceries.shoppinglist.model.ShoppingList;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface ShoppingListRepository {
+public interface ShoppingListRepository extends CrudRepository<ShoppingList, UUID> {
 
 
     ShoppingList save(ShoppingList shoppingList);
